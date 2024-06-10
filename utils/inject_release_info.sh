11a46413bed4ca52dddea30f6ae9cbbc476fa04a
Mikalai Ananenka <ojuuji@gmail.com>
2024-06-11 01:54:28 +0300

    add sha256 link and the release date to the download section

--- utils/inject_release_info.sh
+++ utils/inject_release_info.sh
@@ -1,11 +1,19 @@
-#! /bin/bash -eu
+#! /bin/bash -u
 
-cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+INFO=""
 
 SIZE="$(gh release view latest --json assets --jq '.assets | map(select(.name == "rb.db.xz")) | .[].size')"
-
 if [[ $SIZE -gt 0 ]]; then
 	SIZE=$((SIZE / 1024 * 100 / 1024 ))
-	SIZE="$((SIZE / 100)).$((SIZE % 100)) MiB"
-	sed -i "s/<!--DBXZ_SIZE-->/($SIZE)/" "../docs/_includes/download.html"
+	INFO="$((SIZE / 100)).$((SIZE % 100)) MiB"
+fi
+
+DATE="$(gh release view latest --json name --jq '.name' | grep -Po '\d{4}-\d\d-\d\d \d\d:\d\d')"
+if [[ -n "$DATE" ]]; then
+	[[ -n "$INFO" ]] && INFO="$INFO, $DATE" || INFO="$DATE"
+fi
+
+if [[ -n "$INFO" ]]; then
+	WORKDIR="$(dirname "$(readlink -f "$BASH_SOURCE")")"
+	sed -i "s/<!--DBXZ_INFO-->/($INFO)/" "$WORKDIR/../docs/_includes/download.html"
 fi
