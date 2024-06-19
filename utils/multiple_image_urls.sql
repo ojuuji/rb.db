c4eb056a04dd168d688d08504591224146c02584
Mikalai Ananenka <ojuuji@gmail.com>
2024-06-19 18:53:57 +0300

    elaborate multiple_image_urls report

--- utils/multiple_image_urls.sql
+++ utils/multiple_image_urls.sql
@@ -6,14 +6,34 @@ SELECT datetime(value, 'unixepoch') 'DB version'
   FROM rb_db_lov
  WHERE key = 'data_timestamp';
 
-SELECT DISTINCT set_num, part_num, color_id, img_url
+-- Print parts which have multiple non-NULL image URLs
+
+SELECT part_num, color_id, img_url, count(set_num) num_sets, set_num 'example set_num'
   FROM inventory_parts ip
   JOIN (SELECT part_num, color_id
           FROM inventory_parts
          GROUP BY 1, 2
-        HAVING count(DISTINCT coalesce(img_url, '!')) > 1
+        HAVING count(DISTINCT img_url) > 1
+        -- use this to include null/non-null differences:
+        -- HAVING count(DISTINCT coalesce(img_url, '!')) > 1
        )
  USING (part_num, color_id)
   JOIN inventories i
     ON i.id = ip.inventory_id
- ORDER BY 2, 3, 1;
+ GROUP BY part_num, color_id, img_url
+ ORDER BY 1, 2, 3;
+
+-- Print parts which have NULL and non-NULL image URLs
+
+SELECT part_num, color_id, img_url, count(set_num) num_sets, set_num 'example set_num'
+  FROM inventory_parts ip
+  JOIN (SELECT part_num, color_id
+          FROM inventory_parts
+         GROUP BY 1, 2
+        HAVING count(img_url) > 0 AND count(*) > count(img_url)
+       )
+ USING (part_num, color_id)
+  JOIN inventories i
+    ON i.id = ip.inventory_id
+ GROUP BY part_num, color_id, img_url
+ ORDER BY 1, 2, 3
